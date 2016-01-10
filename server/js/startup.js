442c76f3dce0beebdd609d5c9ce8f46404522c33
sean1rose <sean1rose@gmail.com>
2016-01-09 17:13:19 -0800

    More commented code

--- server/js/startup.js
+++ server/js/startup.js
@@ -1,5 +1,5 @@
-// to ensure that a user does not mistakenly follow the same user twice...
+// Meteor startup runs when server 1st starts up; ensureIndex is a mongodb operation to create an index & insure uniqueness
 Meteor.startup(function(){
-  // Meteor startup runs when server 1st starts up; ensureIndex is a mongodb operation to create an index & insure uniqueness
+  // to ensure that a user does not mistakenly follow the same user twice...
   Relationships._ensureIndex({follower: 1, following: 1}, {unique: 1});
 });
