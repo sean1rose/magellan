5d7acc142c7a521b96b28ca651ae80e9a4e33071
sean1rose <sean1rose@gmail.com>
2016-01-11 22:17:38 -0800

    User management table set up

--- server/js/publications.js
+++ server/js/publications.js
@@ -1,23 +1,5 @@
 // terminal
 
-/*
-// with autopublish removed, need to define what data should be available to users of the application
-// 'tweets' becomes the name of the published data, which is basically all data from Tweets collection
-Meteor.publish('tweets', function(){
-  // this would return all tweets...
-  // return Tweets.find();
-
-  // but want tweets from only those people you are following...
-  if (this.userId){
-    var username = Meteor.users.findOne({_id: this.userId}).username;
-    var currentFollowings = UserUtils.findFollowings(username);
-      // if don't want to add own tweets to list of published tweets, then remove...
-    currentFollowings.push(username);
-    return Tweets.find({user: { $in: currentFollowings}});
-  }
-});
-*/
-
 Meteor.publishComposite('tweets', function(username){
   return {
     // watches for changes in the Relationships db
@@ -51,3 +33,7 @@ Meteor.publish('users', function(username){
 Meteor.publish('followings', function(username){
   return Relationships.find({ follower: username });
 });
+
+Meteor.publish('followers', function(username){
+  return Relationships.find( { following: username });
+})
